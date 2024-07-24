9f4429ecb8c7fe5123a22aa8064d2aa24a03b109
Noah <celestiarune@icloud.com>
2024-07-24 11:25:27 +0900

    Added

---
+++ Split/Split/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ExcelSplitter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtRowCount = new System.Windows.Forms.TextBox();
+            this.lblRowCount = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.lblStartRow = new System.Windows.Forms.Label();
+            this.txtStartRow = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblSelectedFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(150, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select Excel File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+
+            // 
+            // txtRowCount
+            // 
+            this.txtRowCount.Location = new System.Drawing.Point(117, 50);
+            this.txtRowCount.Name = "txtRowCount";
+            this.txtRowCount.Size = new System.Drawing.Size(100, 20);
+            this.txtRowCount.TabIndex = 1;
+
+            // 
+            // lblRowCount
+            // 
+            this.lblRowCount.AutoSize = true;
+            this.lblRowCount.Location = new System.Drawing.Point(12, 53);
+            this.lblRowCount.Name = "lblRowCount";
+            this.lblRowCount.Size = new System.Drawing.Size(76, 13);
+            this.lblRowCount.TabIndex = 2;
+            this.lblRowCount.Text = "Rows per file:";
+
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 150);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(150, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+
+            // 
+            // lblStartRow
+            // 
+            this.lblStartRow.AutoSize = true;
+            this.lblStartRow.Location = new System.Drawing.Point(12, 85);
+            this.lblStartRow.Name = "lblStartRow";
+            this.lblStartRow.Size = new System.Drawing.Size(82, 13);
+            this.lblStartRow.TabIndex = 4;
+            this.lblStartRow.Text = "Data Start Row:";
+
+            // 
+            // txtStartRow
+            // 
+            this.txtStartRow.Location = new System.Drawing.Point(117, 82);
+            this.txtStartRow.Name = "txtStartRow";
+            this.txtStartRow.Size = new System.Drawing.Size(100, 20);
+            this.txtStartRow.TabIndex = 5;
+
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 115);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(205, 23);
+            this.progressBar1.TabIndex = 6;
+
+            // 
+            // lblSelectedFile
+            // 
+            this.lblSelectedFile.AutoSize = true;
+            this.lblSelectedFile.Location = new System.Drawing.Point(168, 17);
+            this.lblSelectedFile.Name = "lblSelectedFile";
+            this.lblSelectedFile.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectedFile.TabIndex = 7;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 181);
+            this.Controls.Add(this.lblSelectedFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtStartRow);
+            this.Controls.Add(this.lblStartRow);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.lblRowCount);
+            this.Controls.Add(this.txtRowCount);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Excel Splitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtRowCount;
+        private System.Windows.Forms.Label lblRowCount;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label lblStartRow;
+        private System.Windows.Forms.TextBox txtStartRow;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblSelectedFile;
+    }
+}
---
+++ Split/Split/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace ExcelSplitter
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnSelectFile.Click += BtnSelectFile_Click;
+            btnProcess.Click += BtnProcess_Click;
+        }
+
+        private void BtnSelectFile_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePath = openFileDialog.FileName;
+                    lblSelectedFile.Text = Path.GetFileName(selectedFilePath);
+                }
+            }
+        }
+
+        private void ResetForm()
+        {
+            txtRowCount.Text = string.Empty;
+            txtStartRow.Text = string.Empty;
+            progressBar1.Value = 0;
+            lblSelectedFile.Text = string.Empty;
+        }
+
+        private async void BtnProcess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFilePath) || string.IsNullOrEmpty(txtRowCount.Text) || string.IsNullOrEmpty(txtStartRow.Text))
+            {
+                MessageBox.Show("Please select an Excel file and enter the number of rows per file and the data start row.");
+                return;
+            }
+
+            if (!int.TryParse(txtRowCount.Text, out int rowCount))
+            {
+                MessageBox.Show("Please enter a valid number for rows per file.");
+                return;
+            }
+
+            if (!int.TryParse(txtStartRow.Text, out int startRow))
+            {
+                MessageBox.Show("Please enter a valid number for the data start row.");
+                return;
+            }
+
+            progressBar1.Value = 0;
+            progressBar1.Maximum = (int)Math.Ceiling((double)(ReadRowCount(selectedFilePath) - startRow + 1) / rowCount);
+
+            await Task.Run(() => SplitAndSaveExcel(selectedFilePath, startRow, rowCount));
+            MessageBox.Show("Files have been successfully created.");
+        }
+
+        private int ReadRowCount(string filePath)
+        {
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheet(1);
+                return worksheet.LastRowUsed().RowNumber();
+            }
+        }
+
+        private void SplitAndSaveExcel(string filePath, int startRow, int rowCount)
+        {
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheet(1);
+                var lastRow = worksheet.LastRowUsed().RowNumber();
+
+                int fileIndex = 1;
+                for (int start = startRow; start <= lastRow; start += rowCount)
+                {
+                    using (var newWorkbook = new XLWorkbook())
+                    {
+                        var newWorksheet = newWorkbook.AddWorksheet("서식");
+
+                        // Copy the header rows
+                        for (int row = 1; row < startRow; row++)
+                        {
+                            for (int col = 1; col <= worksheet.LastColumnUsed().ColumnNumber(); col++)
+                            {
+                                var cell = worksheet.Cell(row, col);
+                                var newCell = newWorksheet.Cell(row, col);
+                                newCell.Value = cell.Value;
+                                newCell.Style = cell.Style;
+                                if (cell.HasFormula)
+                                {
+                                    newCell.FormulaA1 = cell.FormulaA1;
+                                }
+                            }
+                        }
+
+                        // Copy merged cells in header rows
+                        foreach (var merge in worksheet.MergedRanges)
+                        {
+                            if (merge.FirstRow().RowNumber() < startRow)
+                            {
+                                newWorksheet.Range(merge.RangeAddress.FirstAddress.ToString(), merge.RangeAddress.LastAddress.ToString()).Merge();
+                            }
+                        }
+
+                        // Copy the data rows
+                        int newRow = startRow;
+                        for (int row = start; row < start + rowCount && row <= lastRow; row++, newRow++)
+                        {
+                            for (int col = 1; col <= worksheet.LastColumnUsed().ColumnNumber(); col++)
+                            {
+                                var cell = worksheet.Cell(row, col);
+                                var newCell = newWorksheet.Cell(newRow, col);
+                                newCell.Value = cell.Value;
+                                newCell.Style = cell.Style;
+                                if (cell.HasFormula)
+                                {
+                                    newCell.FormulaA1 = cell.FormulaA1;
+                                }
+                            }
+                        }
+
+                        // Copy merged cells in data rows
+                        foreach (var merge in worksheet.MergedRanges)
+                        {
+                            if (merge.FirstRow().RowNumber() >= start && merge.FirstRow().RowNumber() < start + rowCount)
+                            {
+                                int mergeStartRow = merge.FirstRow().RowNumber() - start + startRow;
+                                int mergeEndRow = merge.LastRow().RowNumber() - start + startRow;
+                                newWorksheet.Range(merge.FirstColumn().ColumnLetter() + mergeStartRow, merge.LastColumn().ColumnLetter() + mergeEndRow).Merge();
+                            }
+                        }
+
+                        string newFileName = Path.GetFileNameWithoutExtension(filePath) + $"_{fileIndex}.xlsx";
+                        string newFilePath = Path.Combine(Path.GetDirectoryName(filePath), newFileName);
+                        newWorkbook.SaveAs(newFilePath);
+                        fileIndex++;
+
+                        // Update progress bar
+                        this.Invoke(new Action(() =>
+                        {
+                            progressBar1.Value++;
+                        }));
+                    }
+                }
+            }
+        }
+    }
+}
